8e316d415cce394cc96634260ce890a05b402658
TonsOfStuff <jtons2008@gmail.com>
2024-11-06 21:53:40 -0600

    raycasting test

--- threeJSFirst/main.js
+++ threeJSFirst/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { color } from 'three/webgpu';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,7 +18,10 @@ pointLight.position.set(0,0,1)
 scene.add(pointLight)
 
 const cube = new THREE.Mesh(geometry, material);
+cube.name = "s";
 scene.add(cube);
+
+
 
 camera.position.z = 7;
 
@@ -30,6 +34,33 @@ const gridHelper = new THREE.GridHelper(200,50);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(gridHelper, lightHelper)
 
+
+
+//Raycasting
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseMove(event) {
+  // Calculate mouse position in normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1; 
+}
+
+function onMouseDown(event) {
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children, false);
+
+  if (intersects.length > 0) {
+    console.log('Clicked on:', intersects[0].object.name);
+  }
+}
+
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('mousedown', onMouseDown, false);
+
+
+//Main loop
 function animate(){
     requestAnimationFrame(animate);
 
